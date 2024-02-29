45707550a6bb5abab9ed076427f42ecb86727a28
anthule123 <161700104+an-pink@users.noreply.github.com>
2024-02-29 17:48:55 +0700

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from '/src/App.jsx'
+import '/src/index.css'
+import { createBrowserRouter } from "react-router-dom";
+import Home from '/src/pages/Home.jsx'
+import Tips from '/src/pages/Tips.jsx'
+import { RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/hoho/",
+    element: <App />,
+    children: [
+      {
+        path: "/hoho/",
+        element: <Home />,
+      },
+      {
+        path: "/hoho/tips",
+        element: <Tips />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
